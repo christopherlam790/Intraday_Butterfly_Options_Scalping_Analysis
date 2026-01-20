27dab001f5638df7551e3dac6443f072813256ed
ChrisLam790 <christopherlam790@gmail.com>
2026-01-20 16:17:05 -0500

    2024 data

--- src/main.py
+++ src/main.py
@@ -27,10 +27,10 @@ def load_json(json_path):
     
     
 if __name__ == "__main__":
-    data = load_json(json_path="assets/2025_5_minute/12_2025_5_minute_SPY.json")
+    data = load_json(json_path="assets/2024_5_minute/12_2024_5_minute_SPY.json")
     
     df = pd.DataFrame(data)
-    
+        
     df["datetime"] = pd.to_datetime(df["t"], unit="ms", utc=True)
     df["datetime_est"] = df["datetime"].dt.tz_convert("America/New_York")
     
